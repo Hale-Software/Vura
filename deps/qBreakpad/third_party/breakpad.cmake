b54d61944130d4ad47158822ff78e0cad924045e
Andrew Hale <halea2196@gmail.com>
2026-03-15 18:29:24 -0400

    Added deps/qBreakpad dependency files

---
+++ deps/qBreakpad/third_party/breakpad.cmake
@@ -0,0 +1,74 @@
+# 1. Path Setup
+#get_filename_component(XXHASH_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../.." ABSOLUTE)
+
+set(BREAKPAD_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../third_party/breakpad/src")
+
+# 2. Include Directories
+target_include_directories(qBreakpad PUBLIC
+        ${BREAKPAD_PATH}
+        ${CMAKE_CURRENT_SOURCE_DIR}/../
+)
+
+# 3. Unix-specific (Linux + macOS)
+if(UNIX)
+    target_sources(qBreakpad PRIVATE
+            ${BREAKPAD_PATH}/client/minidump_file_writer.cc
+            ${BREAKPAD_PATH}/common/convert_UTF.cc
+            ${BREAKPAD_PATH}/common/md5.cc
+            ${BREAKPAD_PATH}/common/string_conversion.cc
+    )
+endif()
+
+# 4. macOS-specific
+if(APPLE)
+    # Enable Objective-C++ support for .mm files
+    enable_language(OBJCXX)
+
+    target_sources(qBreakpad PRIVATE
+            ${BREAKPAD_PATH}/client/mac/crash_generation/crash_generation_client.cc
+            ${BREAKPAD_PATH}/client/mac/handler/breakpad_nlist_64.cc
+            ${BREAKPAD_PATH}/client/mac/handler/dynamic_images.cc
+            ${BREAKPAD_PATH}/client/mac/handler/exception_handler.cc
+            ${BREAKPAD_PATH}/client/mac/handler/minidump_generator.cc
+            ${BREAKPAD_PATH}/common/mac/MachIPC.mm
+            ${BREAKPAD_PATH}/common/mac/bootstrap_compat.cc
+            ${BREAKPAD_PATH}/common/mac/file_id.cc
+            ${BREAKPAD_PATH}/common/mac/macho_id.cc
+            ${BREAKPAD_PATH}/common/mac/macho_utilities.cc
+            ${BREAKPAD_PATH}/common/mac/macho_walker.cc
+            ${BREAKPAD_PATH}/common/mac/string_utilities.cc
+    )
+endif()
+
+# 5. Linux-only (Unix but NOT Apple)
+if(UNIX AND NOT APPLE)
+    target_sources(qBreakpad PRIVATE
+            ${BREAKPAD_PATH}/client/linux/crash_generation/crash_generation_client.cc
+            ${BREAKPAD_PATH}/client/linux/dump_writer_common/thread_info.cc
+            ${BREAKPAD_PATH}/client/linux/dump_writer_common/ucontext_reader.cc
+            ${BREAKPAD_PATH}/client/linux/handler/exception_handler.cc
+            ${BREAKPAD_PATH}/client/linux/handler/minidump_descriptor.cc
+            ${BREAKPAD_PATH}/client/linux/log/log.cc
+            ${BREAKPAD_PATH}/client/linux/microdump_writer/microdump_writer.cc
+            ${BREAKPAD_PATH}/client/linux/minidump_writer/linux_core_dumper.cc
+            ${BREAKPAD_PATH}/client/linux/minidump_writer/linux_dumper.cc
+            ${BREAKPAD_PATH}/client/linux/minidump_writer/linux_ptrace_dumper.cc
+            ${BREAKPAD_PATH}/client/linux/minidump_writer/minidump_writer.cc
+            ${BREAKPAD_PATH}/common/linux/breakpad_getcontext.S
+            ${BREAKPAD_PATH}/common/linux/elfutils.cc
+            ${BREAKPAD_PATH}/common/linux/file_id.cc
+            ${BREAKPAD_PATH}/common/linux/guid_creator.cc
+            ${BREAKPAD_PATH}/common/linux/linux_libc_support.cc
+            ${BREAKPAD_PATH}/common/linux/memory_mapped_file.cc
+            ${BREAKPAD_PATH}/common/linux/safe_readlink.cc
+    )
+endif()
+
+# 6. Windows-specific
+if(WIN32)
+    target_sources(qBreakpad PRIVATE
+            ${BREAKPAD_PATH}/client/windows/crash_generation/crash_generation_client.cc
+            ${BREAKPAD_PATH}/client/windows/handler/exception_handler.cc
+            ${BREAKPAD_PATH}/common/windows/guid_string.cc
+    )
+endif()
