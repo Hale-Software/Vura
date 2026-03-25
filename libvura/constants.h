7e21036c172864c984af157b9cc99c9107025052
Andrew Hale <halea2196@gmail.com>
2026-03-24 23:15:47 -0400

    Created constants files

---
+++ libvura/constants.h
@@ -0,0 +1,30 @@
+/*******************************************************************************
+     Copyright (c) 2026.  by Andrew Hale <halea2196@gmail.com>
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#pragma once
+
+#include <string>
+
+
+namespace constants {
+    extern const std::string ApplicationDebugFolder;
+    extern const std::string MediaFileExtensions;
+    extern const std::string VideoFileExtensions;
+    extern const std::string AudioFileExtensions;
+    extern const std::string ApplicationFileExtensions;
+    extern const std::string PlaylistFileExtensions;
+}
