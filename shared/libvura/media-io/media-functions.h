ab7d59342518b7ed653be77046d93f55b5e6e295
Andrew Hale <halea2196@gmail.com>
2026-05-08 13:25:58 -0400

    Refactored functions

--- shared/libvura/media-io/media-functions.h
+++ shared/libvura/media-io/media-functions.h
@@ -1,3 +1,21 @@
+/*******************************************************************************
+     Copyright (c) 2026. by Andrew Hale <halea2196@gmail.com>
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
 #pragma once
 
 #include <QObject>
@@ -24,10 +42,10 @@ public:
     void extractSubclipFromVideo(const QString &filename, int in, int out);
     static QString createTimestampString(qint64 pos);
     static QString generateSubclipFilenameWithIncrement(const QString &directoryPath, const QString &baseFileName, const QString &extension);
-    bool isPlaylist(const QUrl &url);
-    QString strippedFileName(const QString &fileName);
-    QString timestampString(qint64 position, qint64 duration);
-    void takeSnapshot(const QString &filename, const int &position, const QVideoFrame &frame);
+    static bool isPlaylist(const QUrl &url);
+    static QString strippedFileName(const QString &fileName);
+    static QString timestampString(qint64 position, qint64 duration);
+    static void takeSnapshot(const QString &filename, const int &position, const QVideoFrame &frame);
 
 };
 
