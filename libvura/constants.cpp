0943313e21c8b5511b78eb06953356e8c5cf158c
Andrew Hale <halea2196@gmail.com>
2026-06-06 02:31:32 -0400

    Fixed small errors in audio file extensions and application file extensions

--- libvura/constants.cpp
+++ libvura/constants.cpp
@@ -26,13 +26,13 @@ QString _videoFileExtensions = R"(
 )";
 
 QString _audioFileExtensions = R"(
-*.3ga *.669 *.a52 *.aac * *.ac3 .adt *.adts *.aif *.aifc *.aiff *.amb *.amr *.aob *.ape *.au *.awb *.caf *.dts *.flac
+*.3ga *.669 *.a52 *.aac *.ac3 *.adt *.adts *.aif *.aifc *.aiff *.amb *.amr *.aob *.ape *.au *.awb *.caf *.dts *.flac
  *.it *.kar *.m4a *.m4b *.m4p *.m5p *.mid *.mka *.mlp *.mod *.mpa *.mp1 *.mp2 *.mp3 *.mpc *.mpga *.mus *.oga *.ogg
  *.oma *.opus *.qcp *.ra *.rmi *.s3m *.sid *.spx *.tak *.thd *.tta *.voc *.vqf *.w64 *.wav *.wma *.wv *.xa *.xm
 )";
 
 QString _applicationFileExtensions = R"(
-*.vhk *.vprj *vvm
+*.vhk *.vprj *.vvm
 )";
 
 QString _playlistFileExtensions = R"(
